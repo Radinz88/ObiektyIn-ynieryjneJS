a4697ac2e24ef398317add6c09254cba9fbc7d1e
Radosław <613162@stud.umk.pl>
2023-09-06 23:21:02 +0200

    HamburgerMenu update

--- frontend/portal-izb/src/components/LoginBar/HamburgerMenu.jsx
+++ frontend/portal-izb/src/components/LoginBar/HamburgerMenu.jsx
@@ -1,29 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 function HamburgerMenu() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
-    <div className="hamburger-menu">
-      <div className="bars" onClick={toggleMenu}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
+    <nav className="hamburger-menu">
+      <button className="burger">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </button>
+
+      <ul className="menu-items">
+        <li><Link>Zaloguj się</Link></li>
+        <li><Link>Zarejestuj się</Link></li>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link>Aktualności</Link></li>
+        <li><Link>Forum</Link></li>
+        <li><Link>Literatura</Link></li>
+        <li><Link>Sklep</Link></li>
+        <li><Link to="/contact">Kontakt</Link></li>
+      </ul>
       
-      {isMenuOpen && (
-        <div className="menu-items">
-          <Link to="/login">Zaloguj się</Link>
-          <Link to="/register">Zarejestruj się</Link>
-          <Link to="/">Home</Link>
-        </div>
-      )}
-    </div>
+    </nav>
   );
 }
 
